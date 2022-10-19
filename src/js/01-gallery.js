d92245a95a1b5e38fc88585ac4e19cc4c7a743c1
Lidiia Popova <pl.95kh@gmail.com>
2022-10-19 20:56:02 +0300

    /

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,17 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import '../css/common.css';
+import '../css/01-gallery.css';
+import createImgCard from './gallery-markup';
 // Change code below this line
+const galleryEl = document.querySelector('.gallery');
 
-console.log(galleryItems);
+const markup = createImgCard(galleryItems);
+galleryEl.insertAdjacentHTML('beforeend', markup);
+
+new SimpleLightbox('.gallery__item', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+import { save, load } from './storage';
 
+const videoEl = document.querySelector('#vimeo-player');
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+const player = new Player(videoEl);
+
+function onPlayData(data) {
+  save(LOCALSTORAGE_KEY, data);
+}
+
+const initPage = load(LOCALSTORAGE_KEY);
+player.on('timeupdate', throttle(onPlayData, 1000));
+
+if (initPage) {
+  const { seconds } = initPage;
+  player.setCurrentTime(seconds);
+}
